1d8bdb386f20dfa994382d84a5be6df84b8e2818
Paweł Jaroszek <pjaroszek@gmail.com>
2021-05-15 12:35:15 +0200

    added tests solution and  tests projects

---
+++ server/tests/Jaroszek.PoC.SignalRChat.AutomatedUiTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jaroszek.PoC.SignalRChat.AutomatedUiTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
